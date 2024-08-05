3bb0c16813fc091828c0d19cecb7ffb8b8c661d3
Shahul Shaik <shahul.shaik1810@gmail.com>
2024-08-05 12:09:32 -0700

    leetcode_prblm_26

---
+++ neetCode_dsa_course/leetCode26_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,9 @@
+def removeDuplicates(nums):
+    left = 1
+    for right in range(1,len(nums)):
+        if nums[right] != nums[right - 1]:
+            nums[left] = nums[right]
+            left += 1
+    return left
+
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
