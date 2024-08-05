f29e4de4232e95b7840850b264194abfda993120
Shahul Shaik <shahul.shaik1810@gmail.com>
2024-08-05 12:14:19 -0700

    leetcode_27

---
+++ neetCode_dsa_course/leetCode27_remove_element.py
@@ -0,0 +1,9 @@
+def removeElement(nums, val):
+    index = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[index] = nums[i]
+            index += 1
+    return index
+
+print(removeElement([3,2,2,2,3],3))
